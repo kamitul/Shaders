d7e8a238966ec97522facc25df3ddd6781bf1f18
Kamil Tulczyjew <kamcio819@gmail.com>
2020-05-07 13:29:45 +0200

    Added speed of animation randomization

--- Assets/AddShader.cs
+++ Assets/AddShader.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,19 +13,32 @@ public class AddShader : MonoBehaviour
     public float sharpness = 1f;
     public Color color;
 
-    private void Awake()
+    private float heightSpeed = 0f;
+    private float blurSpeed = 0f;
+
+    private void Start()
     {
         material = new Material(Shader.Find("Custom/EyeBlink"));
-
+        heightSpeed = UnityEngine.Random.Range(1.5f, 2f);
+        blurSpeed = UnityEngine.Random.Range(1.25f, 1.8f);
     }
 
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-        material.SetFloat("_height", height);
+        material.SetFloat("_height", height  / heightSpeed);
         material.SetFloat("_width", width);
-        material.SetFloat("_blurSize", blur);
+        material.SetFloat("_blurSize", blur * blurSpeed);
         material.SetFloat("_sharpness", sharpness);
         material.SetColor("_Color", color);
         Graphics.Blit(source, destination, material);
+    }
+
+    private void Update()
+    {
+        if(material.GetFloat("_height") == 0)
+        {
+            heightSpeed = UnityEngine.Random.Range(0.7f, 1f);
+            blurSpeed = UnityEngine.Random.Range(0.25f, 0.6f);
+        }
     }
 }
